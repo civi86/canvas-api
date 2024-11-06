22d49b3c6dbad73e11d6f0fd45362b7ed96f1e9f
civi86 <148190264+civi86@users.noreply.github.com>
2024-11-06 09:39:33 +0200

    commit

--- js/functionality.js
+++ js/functionality.js
@@ -1,4 +1,7 @@
+import { c } from './main.js'
+
 export function functionality(){
+<<<<<<< Updated upstream
     const canvas = document.querySelector('canvas');
     const c = canvas.getContext('2d');
 
@@ -12,9 +15,21 @@ export function functionality(){
             this.radius = 20
             this.radians = 0.75
             this.openRate = 0.02
+=======
+    
+    class Player {
+        constructor({position, velocity}) {
+            this.position = position;
+            this.velocity = velocity;
+            this.radius = 30;
+            this.radians = 0.75;
+            this.openRate = 0.02;
+            this.orientation = 0;
+>>>>>>> Stashed changes
         }
 
         draw() {
+<<<<<<< Updated upstream
             c.beginPath()
             c.arc(this.position.x, this.position.y, this.radius, this.radians,
                 Math.PI * 2 - this.radians)
@@ -22,6 +37,34 @@ export function functionality(){
             c.fillStyle = 'yellow'
             c.fill()
             c.closePath()
+=======
+            let startAngle, endAngle;
+
+            switch (this.orientation) {
+                case 0:
+                    startAngle = this.radians;
+                    endAngle = Math.PI * 2 - this.radians;
+                    break;
+                case 1:
+                    startAngle = Math.PI + this.radians;
+                    endAngle = Math.PI - this.radians;
+                    break;
+                case 2:
+                    startAngle = Math.PI * 1.5 + this.radians;
+                    endAngle = Math.PI * 1.5 - this.radians;
+                    break;
+                case 3:
+                    startAngle = Math.PI * 0.5 + this.radians;
+                    endAngle = Math.PI * 0.5 - this.radians;
+                    break;
+            }
+            c.beginPath();
+            c.arc(this.position.x, this.position.y, this.radius, startAngle, endAngle);
+            c.lineTo(this.position.x, this.position.y);
+            c.fillStyle = 'white';
+            c.fill();
+            c.closePath();
+>>>>>>> Stashed changes
         }
 
         update() {
@@ -33,6 +76,24 @@ export function functionality(){
             this.radians += this.openRate
         }
     }
+
+    class Pellet {
+        constructor({position}) {
+            this.position = position;
+            this.radius = 5;
+        }
+        
+        draw() {
+           
+            c.beginPath();
+            c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+            c.fillStyle = 'white';
+            c.fill();
+            c.closePath();
+        }
+    }
+
+    const pellets = []
 
     const player = new Player({
         position: {
@@ -46,15 +107,26 @@ export function functionality(){
 
     })
     function animate() {
+<<<<<<< Updated upstream
         c.clearRect(0, 0, canvas.width, canvas.height);
         requestAnimationFrame(animate)
+=======
+        c.clearRect(player.position.x-30, player.position.y-30, 60, 60);
+        requestAnimationFrame(animate);
+>>>>>>> Stashed changes
         player.update();
+
+        if (player.velocity.x > 0) player.orientation = 0
+        else if (player.velocity.y < 0) player.orientation = 2
+        else if (player.velocity.x < 0) player.orientation = 1
+        else if (player.velocity.y > 0) player.orientation = 3
     }
     animate()
 
     addEventListener('keydown', ({key}) => {
         switch (key) {
             case 'w':
+<<<<<<< Updated upstream
                 player.velocity.y = -1
                 player.velocity.x = 0
                 break
@@ -69,6 +141,22 @@ export function functionality(){
             case 'd':
                 player.velocity.x = 1
                 player.velocity.y = 0
+=======
+                player.velocity.y = -1;
+                player.velocity.x = 0;
+                break
+            case 'a':
+                player.velocity.x = -1;
+                player.velocity.y = 0;
+                break
+            case 's':
+                player.velocity.y = 1;
+                player.velocity.x = 0;
+                break
+            case 'd':
+                player.velocity.x = 1;
+                player.velocity.y = 0;
+>>>>>>> Stashed changes
                 break
         }
     })
--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-import {functionality} from './functionality.js';
-functionality();
+const canvas = document.querySelector('canvas');
 
+<<<<<<< Updated upstream
 //import {map} from './map.js';
 //map();
+=======
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+export const c = canvas.getContext('2d');
+
+import { functionality } from './functionality.js';
+
+import { map } from './map.js';
+
+window.addEventListener('load', () => {
+    map();
+    functionality();
+});
+>>>>>>> Stashed changes
